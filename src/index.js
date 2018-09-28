cadac68c4cf2ef53c911fb9079a5718c37a04f46
Jacob Bare <jacob@limit0.io>
2018-09-28 12:53:02 -0500

    Add mongodb client and run

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-require('./env');
-const wp = require('./wpapi');
+const env = require('./env');
+const pkg = require('../package.json');
+const app = require('./app');
+const mongodb = require('./mongodb');
 
-const { log } = console;
+const { PORT } = env;
 
-const run = async () => {
-  const res = await wp.posts();
-  log(res);
-};
-
-run().catch(e => setImmediate((e) => {
+mongodb.connect().then((client) => {
+  process.stdout.write(`💾 Successful MongoDB connection to ${client.s.url}\n`);
+  app.listen(PORT, () => {
+    process.stdout.write(`🌐 Express app '${pkg.name} v${pkg.version}' listening on port ${PORT}\n`);
+  });
+}).catch(e => setImmediate(() => {
   throw e;
 }));
