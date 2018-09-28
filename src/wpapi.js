f9eb7f3135a5040169310a5b75723f69476c7d17
Jacob Bare <jacob@limit0.io>
2018-09-28 12:52:51 -0500

    Fix lint error

--- src/wpapi.js
+++ src/wpapi.js
@@ -1,7 +1,7 @@
 const WPAPI = require('wpapi');
 const env = require('./env');
 
-const { API_URI } = env
+const { API_URI } = env;
 
 module.exports = new WPAPI({
   endpoint: `${API_URI}/wp-json`,
